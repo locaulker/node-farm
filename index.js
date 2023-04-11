2078148752c284a6701f490d2b69d2aff28c6695
Lawrence Caulker <locaulker@gmail.com>
2023-04-11 02:03:35 -0400

    built simple API

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ console.log("Will read file!")
 
 ///////////////////////////////////////////////////////////////////////
 // http SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => {
   const pathName = req.url
 
@@ -32,10 +36,15 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW")
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT")
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      ContentType: "application/json"
+    })
+    res.end(data)
   } else {
     res.writeHead(404, {
-      'Content-type': 'text/html',
-      'My-own-header': 'Hello-World'
+      "Content-type": "text/html",
+      "My-own-header": "Hello-World"
     })
     res.end("<h1>Page cannot be found!</h1>")
   }
