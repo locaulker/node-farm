1c353724092b51903275dfdb117975b065040419
Lawrence Caulker <locaulker@gmail.com>
2023-04-12 05:45:46 -0400

    completes html templates. resume @L15

--- index.js
+++ index.js
@@ -26,21 +26,42 @@ console.log("Will read file!")
 ///////////////////////////////////////////////////////////////////////
 // http SERVER
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+)
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+)
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+)
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
   const pathName = req.url
 
+  // OVERVIEW PAGE
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW")
+    res.writeHead(200, { "Content-type": "text/html" })
+    res.end(tempOverview)
+
+    // PRODUCT PAGE
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT")
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, {
       ContentType: "application/json"
     })
     res.end(data)
+
+    // NOT FOUND
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
